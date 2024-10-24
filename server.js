b46da26970c5c1ae9254435dfd5db7361be3974e
Maddie Smith <27313501@edu.cbe.ab.ca>
2024-10-24 13:56:53 -0600

    found id in guess

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ let activeSessions={}
 
 server.get('/newgame', (req, res)=>{
     let newID = uuid.v4();
+    let word = req.query;
+    word = word.answer;
     let newGame = {
-        wordToGuess: "apple",
+        wordToGuess: word,
         guesses:[],
         wrongLetters: [],
         closeLetters: [],
@@ -31,6 +33,13 @@ server.get('/gamestate', (req, res)=>{
     res.send({gameState: state});
 })
 
+server.post('/guess', (req, res)=>{
+    let state =activeSessions[req.body.sessionID];
+    console.log(state['guesses']);
+    state['guesses'].push(req.body);
+
+    res.status(201);
+})
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = server;
