c428e0cbb565d1bc3b40ac0d3cb78f466015be20
Maddie Smith <27313501@edu.cbe.ab.ca>
2024-10-29 13:54:57 -0600

    gamestate after guess correct

--- server.js
+++ server.js
@@ -47,23 +47,63 @@ server.get('/gamestate', (req, res)=>{
 })
 
 server.post('/guess', (req, res)=>{
-    let session =req.body.sessionID;
-    let guess = req.body.guess.split('');
-    let letter;
-
-    guess[0] = {
-        result: "CLOSE",
-        value: letter
-    }
-    for (let i = 0; i < 5; i++) {
-        letter = guess[0];
-    }
-
-    activeSessions[session]['guesses'].push(guess);
+    if (activeSessions[req.body.sessionID]) {
+        let copy;
+        let session =req.body.sessionID;
+        let guess = req.body.guess.split('');
+        let letter;
+        let answer = activeSessions[session].wordToGuess.split('');
     
-
-    res.status(201);
-    res.send({gameState: activeSessions[session]});
+        activeSessions[session].remainingGuesses -= 1;
+        
+        for (let i = 0; i < 5; i++) {
+            letter = guess[i];
+            if (letter == answer[i]){
+                guess[i] = {
+                    result: "RIGHT",
+                    value: letter
+                }
+                activeSessions[session].rightLetters.push(letter);
+            } else {
+                for (let j = 0; j < 5; j++) {
+                    copy = 0;
+                    if (letter == answer[j]){
+                        for (let l = 0; l < activeSessions[session].closeLetters.length; l++) {
+                            if (activeSessions[session].closeLetters[l] == letter) {
+                                copy = 1;
+                            }
+                        }
+                        
+                        if (copy != 1){
+                            guess[i] = {
+                                result: "CLOSE",
+                                value: letter
+                            }
+                            activeSessions[session].closeLetters.push(letter);
+                        }
+                    }
+                }
+            }
+            if (letter == guess[i]) {
+                guess[i] = {
+                    result: "WRONG",
+                    value: letter
+                }
+                activeSessions[session].wrongLetters.push(letter);
+            }
+        }
+        
+        activeSessions[session]['guesses'].push(guess);
+        
+        res.status(201);
+        res.send({gameState: activeSessions[session]});
+    } else if (req.body.sessionID){
+        res.status(404);
+        res.send({error: 'Session ID does not exist'});
+    } else {
+        res.status(400);
+        res.send({error: 'Session ID not found'})
+    }
 })
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
